ce767ef1cd9ca4a5109ddae6f98f10fdb5721b7b
luck-hope <ph272323@163.com>
2024-08-22 18:36:45 +0800

    创建了路由

---
+++ src/pages/Month/index.js
@@ -0,0 +1,4 @@
+const Month = () => {
+    return <div>Month </div>
+}
+export default Month 
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+// 绑定路由 导入此组件
+import Layout from "@/pages/Layout";
+import Month from "@/pages/Month";
+import Year from "@/pages/Year";
+import New from "@/pages/New/inde";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Layout/>,
+        children:[ //绑定子路由
+        {
+            index:true,
+            element:<Month/>
+        },
+        {path:'year',
+        element:<Year/>}
+        ] 
+    },
+    {
+        path: 'new',
+        element: <New />
+    }
+])
+export default router
